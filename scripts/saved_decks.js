ec5ff3547194d495dac5d116d45485fac9bbf47c
TheSench <JSench@gmail.com>
2015-11-20 16:06:03 -0500

    Fixed some minor issues with multi-core and moved saved decks to a new file.

---
+++ scripts/saved_decks.js
@@ -0,0 +1,16 @@
+﻿var load_tests = (function () {
+    // Saved decks
+    var deck1 = '';
+    var deck2 = '';
+
+    return function () {
+        // Set up starting decks
+        document.getElementById('cardlist').value = deck1;
+        document.getElementById('cardlist2').value = deck2;
+        // Turn on debug mode
+        document.getElementById('debug').checked = true;
+        // Setup current battlegrounds
+        document.getElementById('battleground_0').checked = true;
+        document.getElementById('battleground_2').checked = true;
+    }
+})();
