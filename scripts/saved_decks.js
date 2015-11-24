6417dc3b5fdf0ed5863de1d0d13012d3682a34df
TheSench <JSench@gmail.com>
2015-11-24 09:42:42 -0500

    - Fixed page-height. - Added functioning deck hashes - Fixed link generation

--- scripts/saved_decks.js
+++ scripts/saved_decks.js
@@ -1,16 +1,13 @@
 ﻿var load_tests = (function () {
     // Saved decks
-    var deck1 = 'Urs(5), Smelted S(4)x3, Fire Devil(4)x3, Swiftfoot(4), Duoshot(4)x2, Outrunner(4)x2, Lightsworn(5), Swell(5)x2, Mega G(5)';
-    var deck2 = 'Groc(6), Smelted S(4)x2, Duoshot(4), Outrunner(4)x3, Leaper(5), Castlerock(5)x2, Wayfarer(5), Chaos T(5)x2, Bedlam(5), Invisible(5), Clawkin E(5)';
+    var TheSench = "Urs(5), Matriarch(5)=1, Fire Devil(4), Smelted Skeleton(4)x3=3, Outrunner(4)x3=1, Duoshot(4)=1, Toxin(5), Plague(5), Mighty Clawkin(5), Chaos Tempest(5)=3, Lilypad(5), Lightsworn(5)";
+    var zGod = "Groc(6), Heroic(5)x2; Fowl Swarm(5), Wind Spirit(5), Swell(5)x2, Garg(5)x2, Rock T(5), Sapling(5), Tsunamari(5), Honeycomb(5), Avenging(6), Blazekin(6), Nixfire(4)";
+    var dryp = "Samael(6), Glass Titan(5), Gravity Bender(5), Firebomb Spirit(5), Mega Garganatos(5), Sage Tsunamari(5), Swamp Aberration(5)x2, Darkness Elemental(5), Majestic Guardian(4), Shining Radiance(4), Brilliant Aurora(4), Rust Goliath(5), Pumpking Kong(5), Rhino Beast(6), Gold Incarnate(6)";
+    var oemse = "Groc(6), Hellfrog(4), Outrunner(4)x4, Lilypad(5), Beacon(5), Blitz(5), Mob(5), Tempest(5), Noxious(5), Great Sage(5), King(5), Blazen D(6), Beat(6)"
+    var AMSP = "Urs(5), Smelted S(4)x3, Fire Devil(4)x3, Swiftfoot(4), Duoshot(4)x2, Outrunner(4)x2, Lightsworn(5), Swell(5)x2, Mega G(5)"
+    var alpha = "Tarian(6), Fire Devil(4)x2, Chaos Swell(5), Chaos Tempest(5), Branding Engine(6), Firebomb Spirit(5), Sage Tsunamari(5), Blazing Aurora(6), Blazen Dragon(6), Onyx Knight(6), Thousand Arm Fiend(6), Pumpking Kong(6), Spirit of Insanity(6)";
+    var golden = "Groc(6), Smelted S(4)x2, Duoshot(4), Outrunner(4)x3, Leaper(5), Castlerock(5)x2, Wayfarer(5), Chaos T(5)x2, Bedlam(5), Invisible(5), Clawkin E(5)"
 
     return function () {
-        // Set up starting decks
-        document.getElementById('cardlist').value = deck1;
-        document.getElementById('cardlist2').value = deck2;
-        // Turn on debug mode
-        document.getElementById('debug').checked = false;
-        // Setup current battlegrounds
-        document.getElementById('battleground_0').checked = true;
-        document.getElementById('battleground_2').checked = true;
     }
 })();
