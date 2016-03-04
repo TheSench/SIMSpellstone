8afda3dd5762e62a5d9729b393bc654243e0d3a2
TheSench <JSench@gmail.com>
2016-03-04 16:14:48 -0500

    New Elemental Card Images.

--- scripts/data/spoilers.js
+++ scripts/data/spoilers.js
@@ -1,13 +1,4 @@
 var spoilers = {};
-spoilers[5043] = true;
-spoilers[15043] = true;
-spoilers[25043] = true;
-spoilers[6042] = true;
-spoilers[16042] = true;
-spoilers[26042] = true;
-spoilers[7042] = true;
-spoilers[17042] = true;
-spoilers[27042] = true;
-spoilers[9999] = true;
-spoilers[19999] = true;
-spoilers[29999] = true;
+spoilers[6043] = true;
+spoilers[16043] = true;
+spoilers[26043] = true;
