3cd2ac5f8edf2a971600199cfffb27683e4be996
TheSench <JSench@gmail.com>
2016-05-12 22:17:21 -0400

    Fixed titan deck hashes

--- scripts/data/titans.js
+++ scripts/data/titans.js
@@ -2,61 +2,61 @@
     "1500": {
         "id": "1500",
         "name": "The Earth Trembles...",
-        "hash": "GD!b9Fb9Fb9Fb9Gb9Gb9GptqptmptmpthpthptaptZptZptg"
+        "hash": "GD!b9Fb9Fb9Fb9Gb9Gb9GptqptmptmpthpthptaptZptZptg/"
     },
     "1501": {
         "id": "1501",
         "name": "The Land Shatters...",
-        "hash": "GD!b9Fb9Fb9Gb9GptqptmptmpthpthptaptaptZptZUfdptg"
+        "hash": "GD!b9Fb9Fb9Gb9GptqptmptmpthpthptaptaptZptZUfdptg/"
     },
     "1502": {
         "id": "1502",
         "name": "Rising from the Depths...",
-        "hash": "GD!b9Fb9Fb9GptqptmptmpthpthptaptaptZptZUfdptgptg"
+        "hash": "GD!b9Fb9Fb9GptqptmptmpthpthptaptaptZptZUfdptgptg/"
     },
     "1503": {
         "id": "1503",
         "name": "Atlas, the Ancient",
-        "hash": "GD!b9Fb9GptqptqptmptmpthpthptaptaptZptZUfdUfdptg"
+        "hash": "GD!b9Fb9GptqptqptmptmpthpthptaptaptZptZUfdUfdptg/"
     },
     "1504": {
         "id": "1504",
         "name": "The Wind Stirs...",
-        "hash": "GD~b9Hb9Hb9Ib9IpOQpOQUfXUfXpOVpOVpOipOipOfpOaUQS"
+        "hash": "GD~b9Hb9Hb9Ib9IpOQpOQUfXUfXpOVpOVpOipOipOfpOaUQS/"
     },
     "1505": {
         "id": "1505",
         "name": "The Clouds Part...",
-        "hash": "GD~b9Hb9Hb9Ib9Ib9IpOQpOQUfXUfXpOVpOVpOipOipOfpOaUQS"
+        "hash": "GD~b9Hb9Hb9Ib9Ib9IpOQpOQUfXUfXpOVpOVpOipOipOfpOaUQS/"
     },
     "1506": {
         "id": "1506",
         "name": "The Air Chills...",
-        "hash": "GD~b9Hb9Ib9Ib9IpOQpOQUfXUfXpOVpOipOipOfpOaUQSUQSUQS"
+        "hash": "GD~b9Hb9Ib9Ib9IpOQpOQUfXUfXpOVpOipOipOfpOaUQSUQSUQS/"
     },
     "1507": {
         "id": "1507",
         "name": "Solaron, The Origin",
-        "hash": "GD~b9Hb9IpOQpOQUfXUfXpOVpOVpOipOipOipOfpOfpOaUQS"
+        "hash": "GD~b9Hb9IpOQpOQUfXUfXpOVpOVpOipOipOipOfpOfpOaUQS/"
     },
     "1508": {
         "id": "1508",
         "name": "The Heat Rises...",
-        "hash": "GEAb9Jb9KUU8UU8Uffpd!pd9pd9pdzpdyUfWpeDpeDpeI"
+        "hash": "GEAb9Jb9KUU8UU8Uffpd!pd9pd9pdzpdyUfWpeDpeDpeI/"
     },
     "1509": {
         "id": "1509",
         "name": "The Lava Flows...",
-        "hash": "GEAb9Jb9Kb9Kb9KUU8UU8Uffpd!pd9pdzpdyUfWpeDpeIpeI"
+        "hash": "GEAb9Jb9Kb9Kb9KUU8UU8Uffpd!pd9pdzpdyUfWpeDpeIpeI/"
     },
     "1510": {
         "id": "1510",
         "name": "The Volcano Erupts...",
-        "hash": "GEAb9Jb9Jb9Jb9KUU8UU8UffUffpd!pd9pdzpdyUfWpeDpeI"
+        "hash": "GEAb9Jb9Jb9Jb9KUU8UU8UffUffpd!pd9pdzpdyUfWpeDpeI/"
     },
     "1511": {
         "id": "1511",
         "name": "Vulcanos",
-        "hash": "GEAb9Jb9Jb9Jb9Jb9Kb9Kb9KUU8Uffpd9pdyUfWpeDpeIodzod!"
+        "hash": "GEAb9Jb9Jb9Jb9Jb9Kb9Kb9KUU8Uffpd9pdyUfWpeDpeIodzod!/"
     },
 }
