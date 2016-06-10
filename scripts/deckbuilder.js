0a6852b8dc9c7f4a3d618b04ab5eab164eabc6bc
TheSench <jsench@gmail.com>
2016-06-10 14:59:47 -0400

    Fixed label for load dialog

--- scripts/deckbuilder.js
+++ scripts/deckbuilder.js
@@ -1764,7 +1764,7 @@ function saveDeck() {
 
 function loadDeck() {
     var decks = storageAPI.getSavedDecks;
-    $('label[for="loadDeckName"]').text('<strong>Deck:</strong>');
+    $('label[for="loadDeckName"]').html('<strong>Deck:</strong>');
     loadDeckDialog.dialog("open");
     loadDeckDialog.dialog("option", "position", { my: "center", at: "center", of: window });
 
@@ -1773,7 +1773,7 @@ function loadDeck() {
 
 function loadInventory() {
     var decks = storageAPI.getSavedDecks;
-    $('label[for="loadDeckName"]').text('<strong>Inventory:</strong>');
+    $('label[for="loadDeckName"]').html('<strong>Inventory:</strong>');
     loadDeckDialog.dialog("open");
     loadDeckDialog.dialog("option", "position", { my: "center", at: "center", of: window });
 
