490529f30f8f62626fc886f4203f7a7822042bcc
TheSench <jsench@gmail.com>
2016-08-16 16:12:13 -0400

    Fixed Object.assign error in IE

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,6 +1,6 @@
 "use strict";
 
-if (!String.prototype.format) {
+if (typeof String.prototype.format !== 'function') {
     String.prototype.format = function () {
         var args = arguments;
         return this.replace(/{(\d+)}/g, function (match, number) {
@@ -9,6 +9,28 @@ if (!String.prototype.format) {
               : match
             ;
         });
+    };
+}
+
+if (typeof Object.assign !== 'function') {
+    Object.assign = function (target) {
+        'use strict';
+        if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        target = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+                for (var key in source) {
+                    if (Object.prototype.hasOwnProperty.call(source, key)) {
+                        target[key] = source[key];
+                    }
+                }
+            }
+        }
+        return target;
     };
 }
 
