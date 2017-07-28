01c0af559267afddebcd3564c4a1a057e2d0c80d
TheSench <jsench@gmail.com>
2017-07-28 09:12:18 -0400

    Removed useless statement

--- scripts/simulator_base.js
+++ scripts/simulator_base.js
@@ -2655,7 +2655,6 @@ var SIMULATOR = {};
                     }
                     leech_health += enhanced;
                 }
-                leech_health = leech_health;
                 var healthMissing = current_assault.health - current_assault.health_left;
                 if (leech_health >= healthMissing) {
                     leech_health = healthMissing;
