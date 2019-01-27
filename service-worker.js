d1168da76fa53a6406b1b6e8d9d2adad868107b2
Jon Senchyna <TheSench@users.noreply.github.com>
2019-01-27 07:16:48 -0500

    Enable offline analytics

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
+workbox.googleAnalytics.initialize();
+
 if (workbox) {
     workbox.routing.registerRoute(
         /.*\.html/,
