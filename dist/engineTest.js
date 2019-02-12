52c396b6a7689a1c59b308efeb1383ffd8d70802
Jon Senchyna <TheSench@users.noreply.github.com>
2019-02-12 07:30:12 -0500

    Engine Test pulls from URL

--- dist/engineTest.js
+++ dist/engineTest.js
@@ -3858,35 +3858,54 @@ var current_timeout;;define('matchStats', [], function() {
 
     var simController = require('simController');
     var matchStats = require('matchStats');
+    var base64 = require('base64');
     var ui = require('ui');
     var matchTimer = require('matchTimer');
+    var urlHelper = require('urlHelper');
 
     ui.getConfiguration = function getConfiguration() {
         return {
+            playerHash: urlHelper.paramValue('deck1'),
+            playerOrdered: urlHelper.paramDefined("ordered"),
+            playerExactOrder: urlHelper.paramDefined("exactorder"),
+
+            cpuHash: urlHelper.paramValue('deck2'),
+            cpuOrdered: urlHelper.paramDefined("ordered2"),
+            cpuExactOrder: urlHelper.paramDefined("exactorder2"),
+
             userControlled: false,
-            cpuExactOrder: false,
-            cpuHash: "QpLQAQcHQBA3KQBAiYQBgsRQBQHVQBIC!wAoJJQB",
-            cpuOrdered: false,
-            enemybges: "",
-            mapbges: "",
-            missionLevel: "7",
-            playerExactOrder: false,
-            playerHash: "QpLQAQcHQBA3KQBAiYQBgsRQBQHVQBIC!wAoJJQB",
-            playerOrdered: false,
-            raidLevel: "25",
-            selectedBges: "121,122",
-            selectedCampaign: "",
-            selectedMission: "",
-            selectedRaid: "",
-            selfbges: "",
-            siegeMode: false,
-            simsToRun: "10000",
-            surgeMode: false,
-            tournamentMode: false,
-            towerLevel: "18",
-            towerType: "501"
+            selectedBges: getBgesFromHash('bges'),
+            selfbges: getBgesFromHash('selfbges'),
+            enemybges: getBgesFromHash('enemybges'),
+            mapbges: getBgesFromHash('mapBges'),
+
+            selectedCampaign: urlHelper.paramValue('campaign'),
+            selectedMission: urlHelper.paramValue('mission'),
+            missionLevel: (urlHelper.paramValue('mission_level') || '7'),
+            selectedRaid: urlHelper.paramValue('raid'),
+            raidLevel: (urlHelper.paramValue('raid_level') || '25'),
+            simsToRun: (urlHelper.paramValue('sims') || "10000"),
+
+            surgeMode: urlHelper.paramDefined("surge"),
+            tournamentMode: urlHelper.paramDefined("tournament"),
+
+            siegeMode: urlHelper.paramDefined("siege"),
+            towerLevel: Math.min(Math.max(urlHelper.paramValue('tower_level') || 18, 0), 18),
+            towerType: (urlHelper.paramValue('tower_type') || 501)
         };
     };
+
+    function getBgesFromHash(paramName) {
+        var hashedBges = urlHelper.paramValue(paramName);
+        var bges = [];
+		if (hashedBges) {
+			// Each BGE is a 2-character ID in Base64
+			for (var i = 0; i < hashedBges.length; i += 2) {
+                bges.push(base64.toDecimal(hashedBges.substring(i, i + 2)));
+            }
+        }
+        return bges.join(',');
+    }
 
     simController.startsim();
 
