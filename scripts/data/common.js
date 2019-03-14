fed0f8b72670ad3b00bda74a5ea2e4406a634bc9
Jon Senchyna <TheSench@users.noreply.github.com>
2019-03-14 17:26:36 -0400

    Auto-update of Spellstone Data - '2019-03-14

--- scripts/data/common.js
+++ scripts/data/common.js
@@ -3,4 +3,4 @@
 var text_version = 'v2.0';
 var battle_sim = false;
 var current_bges = [112,113];
-var DataUpdated = 1552581011920;
+var DataUpdated = 1552598769696;
--- scripts/data/spoilers.js
+++ scripts/data/spoilers.js
@@ -1,9 +1,5 @@
 "use strict"
 
 var spoilers = {
-	"426": true,
-	"5604": true,
-	"7229": true,
-	"17229": true,
-	"27229": true
+	"426": true
 };
