a44156dcf6acdb4250bf4d34e2f8e3cd53baac3b
Jon Senchyna <TheSench@users.noreply.github.com>
2019-05-18 20:35:28 -0400

    Fix caching w/ query strings

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,68 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
 
 workbox.googleAnalytics.initialize();
 
 if (workbox) {
+    function baseFilePath(url) {
+        return url.split('?')[0];
+    }
+    const cacheKeyWillBeUsed = async ({request, mode}) => {
+        console.log("request", request);
+        console.log("mode", mode);
+        return baseFilePath(request.url);
+    };
+
+    const cleanOldCaches = async ({request, response, event}) => {
+        // Return `response`, a different Response object or null
+        var requestedFile = baseFilePath(request.url);
+        caches.open('js-cache').then(cache => {
+            cache.keys().then(keys => {
+                keys.filter(key => baseFilePath(key.url) === requestedFile && key.url != request.url)
+                    .forEach(key => cache.delete(key));
+            });
+        });
+        return response;
+    };
+
     workbox.routing.registerRoute(
         /.*\.html/,
-        workbox.strategies.staleWhileRevalidate({
-            cacheName: 'html-cache'
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'html-cache',
+            plugins: [
+                {cacheKeyWillBeUsed}
+            ]
         })
     );
     workbox.routing.registerRoute(
         /\/dist\/data\.min\.js/,
-        workbox.strategies.staleWhileRevalidate({
+        new workbox.strategies.StaleWhileRevalidate({
             cacheName: 'data-cache'
         })
     );
     workbox.routing.registerRoute(
         /^.*\/((?!(data\.min)).)*\.js/,
-        workbox.strategies.networkFirst({
-            cacheName: 'js-cache'
+        new workbox.strategies.NetworkFirst({
+            cacheName: 'js-cache',
+            plugins: [
+                {cacheWillUpdate: cleanOldCaches}
+            ]
         })
     );
     workbox.routing.registerRoute(
         /.*\.css/,
-        workbox.strategies.cacheFirst({
+        new workbox.strategies.CacheFirst({
             cacheName: 'css-cache'
         })
     );
     workbox.routing.registerRoute(
         /.*\.png/,
-      workbox.strategies.cacheFirst({
+      new workbox.strategies.CacheFirst({
         cacheName: 'image-cache'
       })
     );
     workbox.routing.registerRoute(
         /\/sprites\/.*\.jpg/,
-      workbox.strategies.cacheFirst({
+      new workbox.strategies.CacheFirst({
         cacheName: 'sprite-cache'
       })
     );
