635143f2b26f38bffbca83205369f4269feb2090
Jon Senchyna <TheSench@users.noreply.github.com>
2019-06-29 17:41:56 -0400

    Adding additional tests from refactor

--- cypress/integration/Simulator.spec.js
+++ cypress/integration/Simulator.spec.js
@@ -1,8 +1,10 @@
 const baseUrl = 'http://localhost:8080/Titans.html';
 
 function testDeck(description, deck1, deck2, expectedWinrate) {
+    var sims = (expectedWinrate == 100 || expectedWinrate == 0) ? 50 : 1000;
+
     it(`${description} (${deck1} vs ${deck2}) should result in winrate of ${expectedWinrate}%`, function () {
-        cy.visit(baseUrl + `?deck1=${deck1}&deck2=${deck2}&bges=&sims=1000&autostart`);
+        cy.visit(baseUrl + `?deck1=${deck1}&deck2=${deck2}&sims=${sims}&autostart`);
         cy.get('#simulationStatus')
             .contains('Simulations complete')
             .then(function() {
@@ -23,7 +25,15 @@ describe('Initialization', function () {
     });
 });
 
+describe('Debug Tests', function() {
+    it('doesn\'t crash', function () {
+        cy.visit(baseUrl + '?deck1=QpLQAoAlrBoAlrBoAlrBoAlrBoAlrB&deck2=QpLQAoAlrBoAlrBoAlrBoAlrBoAlrB&debug&autostart');
+        cy.get('#show-turns').should('have.value', 'Show All');
+    });
+});
+
 describe('Deck Tests', function() {
+
     testDeck('(1/1/1) vs (1/1/1) ', 'QpLQA4DowA', 'QpLQA4DowA', 100);
 
     // wins if 1/1/1 is dropped first
@@ -31,4 +41,10 @@ describe('Deck Tests', function() {
 
     testDeck('(1/3/1 armor 1) vs (1/6/0) ', 'QpLQAIFMQB', 'QpLQAQcHQB', 100);
     testDeck('(1/3/1 bolt 1) vs (1/3/1 armor 1) ', 'QpLQAoKOoB', 'QpLQAIFMQB', 100);
+    testDeck('(1/4/1 weaken 1) vs 1/8/0) ', 'QpLQAY7soB', 'QpLQAA6IQB', 100);
+    testDeck('(3/6/1 hex 2,pierce 2) vs 3/5/0) ', 'QpLQAIjRpB', 'QpLQAIPLoB', 100);
+    testDeck('(3/6/1 hex 2,pierce 2) vs 3/9/2 poison 3) ', 'QpLQAIjRpB', 'QpLQAwMXoB', 100);
+    
+    // TODO: Rune test
+    testDeck('(5/4/0 poison 3+2, pierce 6, invis 2) vs (3/10/2 bolt 2, counter 2) ', 'QpLQAEKiqB', 'QpLQAQHVQB', 100);
 });
