2d1f4370111b6510522444fb724fc7e2ae984e5b
Jon Senchyna <TheSench@users.noreply.github.com>
2020-09-09 16:42:31 -0400

    Auto-update of Spellstone Data - '2020-09-09

--- scripts/data/spoilers.js
+++ scripts/data/spoilers.js
@@ -1,14 +1,7 @@
 "use strict"
 
 var spoilers = {
-	"5279": true,
-	"5615": true,
-	"6279": true,
-	"7281": true,
-	"15279": true,
-	"16279": true,
-	"17281": true,
-	"25279": true,
-	"26279": true,
-	"27281": true
+	"6280": true,
+	"16280": true,
+	"26280": true
 };
