7dccc974be4563b8c2968f6c2bcef86a0a6de80c
Jon Senchyna <TheSench@users.noreply.github.com>
2022-11-10 12:41:45 -0500

    Auto-update of Spellstone Data - '2022-11-10

--- scripts/data/common.js
+++ scripts/data/common.js
@@ -2,5 +2,5 @@
 
 var text_version = 'v2.0';
 var battle_sim = false;
-var current_bges = [156,157];
-var DataUpdated = 1668094904875;
+var current_bges = [157,158];
+var DataUpdated = 1668102053002;
