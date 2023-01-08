432cb74cd4aff3a3c6eb974d0e75ebd2212af986
vuzaldo <121903720+vuzaldo@users.noreply.github.com>
2023-01-08 18:51:25 -0300

    Invigorated cards not being reset properly
    card.health_left being set to card.health before "applyDefaultStatuses" (when the invigorated health amount is removed).
    This is causing Atlas to appear slightly stronger than it already is.

--- scripts/shared.js
+++ scripts/shared.js
@@ -163,9 +163,9 @@ function shuffle(list) {
 function initializeCard(card, p, newKey) {
     card.owner = p;
     card.timer = card.cost;
-    card.health_left = card.health;
     // Setup status effects
     applyDefaultStatuses(card);
+    card.health_left = card.health;
     card.key = newKey;
     if (!card.reusableSkills) card.resetTimers();
 }
