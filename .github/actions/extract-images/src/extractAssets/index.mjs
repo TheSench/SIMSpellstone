140946b8c27e080969bfb6c2486c49a63d598ae3
Jon Senchyna <TheSench@users.noreply.github.com>
2023-03-27 13:40:13 -0400

    feat: Add script to extract assets from image

---
+++ .github/actions/extract-images/src/extractAssets/index.mjs
@@ -0,0 +1,35 @@
+import { execFile } from 'child_process';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+import { pathFromRoot } from '../rootDir.mjs';
+import { existsSync } from 'fs';
+
+function main() {
+    const downloadsDir = pathFromRoot('Downloads');
+    readdirSync(downloadsDir)
+        .filter(filename => filename.endsWith('.unity3d'))
+        .forEach(fileName => extract(downloadsDir, fileName));
+}
+
+function extract(downloadsDir, fileName) {
+    const filePath = join(downloadsDir, fileName);
+    const extractedName = fileName.replace('.unity3d', '');
+
+    if (!existsSync(join(downloadsDir, extractedName))) {
+        console.log(`${fileName} -> ${extractedName}`);
+        execFile(
+            pathFromRoot('.venv', 'Scripts', 'python.exe'),
+            [pathFromRoot('.github', 'actions', 'extract-images', 'disunity.py'), filePath],
+            {
+                cwd: downloadsDir,
+            },
+            (error, _stdout, stderr) => {
+                if (error) {
+                    throw new Error(stderr);
+                }
+            }
+        );
+    }
+}
+
+main();
