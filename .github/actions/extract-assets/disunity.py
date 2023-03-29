7ea4a5e579144b17232a02184a2da485f5d3f251
Jon Senchyna <TheSench@users.noreply.github.com>
2023-03-29 07:58:14 -0400

    feat: Add some exception logging to disunity.py

--- .github/actions/extract-assets/disunity.py
+++ .github/actions/extract-assets/disunity.py
@@ -1,6 +1,7 @@
 
 import UnityPy
 import sys, os, json
+import logging
 
 file = sys.argv[-1] # last argument
 out = file[:file.rindex('\\') + 1] # output path
@@ -19,12 +20,17 @@ for obj in bundle.objects:
 		mono.append(obj)
 
 for obj in mono:
-	data = obj.read_typetree()
-	if not data.get('textures'): continue
-	texture_id = data['textures'][0]['m_PathID']
-	if texture_id not in textures: continue
-	texture = textures[texture_id]
-	os.makedirs(out, exist_ok = True)
-	name = data['spriteCollectionName']
-	with open(out + name + '.json', 'w') as f: json.dump(data, f, indent = 4)
-	texture.save(out + name + '.png')
+	try:
+		data = obj.read_typetree()
+		if not data.get('textures'): continue
+		texture_id = data['textures'][0]['m_PathID']
+		if texture_id not in textures: continue
+		texture = textures[texture_id]
+		os.makedirs(out, exist_ok = True)
+		name = data['spriteCollectionName']
+		with open(out + name + '.json', 'w') as f: json.dump(data, f, indent = 4)
+		texture.save(out + name + '.png')
+	except Error as ex:
+		print('Error while extracting', obj.path_id)
+		logging.exception(ex)
+		raise
