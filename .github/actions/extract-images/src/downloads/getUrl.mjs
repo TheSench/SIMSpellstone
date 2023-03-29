636645abd9749cf6129a04978d681c4d6c62a36a
Jon Senchyna <TheSench@users.noreply.github.com>
2023-03-28 22:34:30 -0400

    fix: Update file URLs

--- .github/actions/extract-images/src/downloads/getUrl.mjs
+++ .github/actions/extract-images/src/downloads/getUrl.mjs
@@ -1,5 +1,6 @@
-const baseURL = "https://d3splaxnu2bep2.cloudfront.net/spellstone/asset_bundles_live/2020_3_33f1/";
-const fileVersion = "_unity2020_3_33_webgl.unity3d";
+const baseURL = "https://d3splaxnu2bep2.cloudfront.net/spellstone/asset_bundles/2020_3_42f1/";
+
+const fileVersion = "_unity2020_3_42_webgl.unity3d";
 
 export function getUrl(fileName) {
     return (baseURL + fileName + fileVersion);
