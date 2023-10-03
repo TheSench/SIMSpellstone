8a1195dec82e4237c566112b1c19b6beb9c06e78
Jon Senchyna <TheSench@users.noreply.github.com>
2023-10-03 10:12:53 +0000

    Auto-update of Spellstone Data - 2023-10-03

--- scripts/data/spoilers.js
+++ scripts/data/spoilers.js
@@ -1,13 +1,5 @@
 "use strict"
 
 var spoilers = {
-	"484": true,
-	"485": true,
-	"486": true,
-	"487": true,
-	"488": true,
-	"489": true,
-	"1625": true,
-	"11625": true,
-	"21625": true
+	"484": true
 };
